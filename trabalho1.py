a74839ee6ac00bfbcf701bf56c8ad333c0d34b42
Joaquim Borges <103827510+joaquimborges-pt@users.noreply.github.com>
2022-11-21 22:43:32 +0000

    Update trabalho1.py
    more changes

--- trabalho1.py
+++ trabalho1.py
@@ -12,22 +12,41 @@ def menu() -> None:
     print("3 - Primo de Mersenne")
     print("4 - Número Harmónico")
     opcao = int(input("Insira a opção desejada:\n"))
-    dict_fun[opcao]()
+    dict_fun[opcao: int]()
 
 
 def pitagoras() -> None:
+    from math import sqrt
     print("Teorema de Pitágoras")
+    catet1 = int(input("Introduza o comprimento do primeiro cateto:\n"))
+    catet2 = int(input("Intoduza o comprimento do segundo cateto:\n"))
+    hipotenusa = sqrt(catet1**2 + catet2**2)
+    print(f'A hipotenusa do triângulo com catetos {catet1} e {catet2} é igual a ', hipotenusa)
 
 def resolvente() -> None:
+    from math import sqrt
     print("Fórmula Resolvente")
-
+    a = int(input("Introduza o valor de a:\n"))
+    b = int(input("Intoduza o valor de b:\n"))
+    c = int(input("Introduza o valor de c:\n"))
+    factor = b**2 - 4*a*c
+    if factor >= 0:
+        x1 = sqrt(factor) - b
+        x2 = -1*(sqrt(factor) + b)
+        print(f'As raizes da equação com a={a}, b={b} e c={c} são: x1={x1} e x2={x2}')
+    else:
+        print("Não existem soluções reais.")
 
 def mersenne() -> None:
+    from math import sqrt
     print("Primo de Mersenne")
+    num = int(input("Introduza um numero de n:\n"))
 
 
 def harmonico() -> None:
+    from math import sqrt
     print("Número Harmónico")
+    num = int(input("Introduza um numero de n:\n"))
 
 
 

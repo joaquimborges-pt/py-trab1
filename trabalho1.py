c8a6e5f6e4bda35a78d443e17c5cab2b13f58495
Joaquim Borges <joaquim.borges@newdecision.pt>
2022-11-21 19:40:10 +0000

    few changes

--- trabalho1.py
+++ trabalho1.py
@@ -5,20 +5,29 @@ def main() -> None:
     menu()
 
 def menu() -> None:
+    dict_fun = {1: pitagoras, 2 : resolvente, 3 : mersenne, 4:harmonico}
     print("MENU")
     print("1 - Teorema de Pitágoras")
     print("2 - Fórmula Resolvente")
     print("3 - Primo de Mersenne")
     print("4 - Número Harmónico")
     opcao = int(input("Insira a opção desejada:\n"))
-    test_dict = {1: "pitagoraica", 2 : "resolvent", 3 : "mersenne", 4:"hramonico"}
-    print(test_dict[1])
-    #print("opcao:", opcao)
+    dict_fun[opcao]()
 
 
+def pitagoras() -> None:
+    print("Teorema de Pitágoras")
+
+def resolvente() -> None:
+    print("Fórmula Resolvente")
 
 
+def mersenne() -> None:
+    print("Primo de Mersenne")
 
+
+def harmonico() -> None:
+    print("Número Harmónico")
 
 
 
